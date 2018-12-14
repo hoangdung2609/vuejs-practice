9f1652fdc913cb00d13a909fb4842346a779f993
dungha <dungha@dreamlab.com.vn>
2018-12-14 17:32:35 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
     <meta id="csrf-token" name="csrf-token" value="{{ csrf_token() }}">
 </head>
 <body>
+<div id="app">
+
+</div>
 <div id="app-2">
   <span v-bind:title="message">
     Hover your mouse over me for a few seconds
@@ -17,23 +20,59 @@
 </div>
 <div id="app-1" v-bind:title="message">
     <span>
-        xxxxxxxxxxxxx
+        @{{ message }}
     </span>
 </div>
+
+<div id="app-3" v-if="seen">
+    <h1>TRUE !!!</h1>
+</div>
+
+<div id="app-4">
+    <ol>
+        <li v-for="todo in todos">
+            {{ todo.text }}
+        </li>
+    </ol>
+</div>
+
 <script src="{{ mix('js/app.js') }}"></script>
 <script>
+    var app4 = new Vue({
+        el: '#app-4',
+        data: {
+            todos: [
+                { text: 'Learn JavaScript' },
+                { text: 'Learn Vue' },
+                { text: 'Build something awesome' }
+            ]
+        }
+    })
+
+    var app3 = new Vue({
+
+    });
+
     var app1 = new Vue({
        el: '#app-1',
        data: {
            message: 'Hello Vue!'
        }
     });
+
     var app2 = new Vue({
         el: '#app-2',
         data: {
             message: 'You loaded this page on ' + new Date().toLocaleString()
         }
     })
+
+    var app3 = new Vue({
+        el: '#app-3',
+        data: {
+            seen: true
+        }
+    });
 </script>
 </body>
 </html>
