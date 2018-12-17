f459c0660c25b57cfc277639fbfbc127f470f71b
dungha <dungha@dreamlab.com.vn>
2018-12-17 17:21:30 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,70 +9,44 @@
     <meta id="csrf-token" name="csrf-token" value="{{ csrf_token() }}">
 </head>
 <body>
-<div id="app">
-
-</div>
-<div id="app-2">
-  <span v-bind:title="message">
-    Hover your mouse over me for a few seconds
-    to see my dynamically bound title!
-  </span>
-</div>
-<div id="app-1" v-bind:title="message">
-    <span>
-        @{{ message }}
-    </span>
-</div>
-
-<div id="app-3" v-if="seen">
-    <h1>TRUE !!!</h1>
-</div>
-
-<div id="app-4">
-    <ol>
-        <li v-for="todo in todos">
-            {{ todo.text }}
-        </li>
-    </ol>
+<div id="app"></div>
+<div id="app1">
+    <p>@{{ foo }}</p>
+    <button v-on:click="created()">Change it</button>
 </div>
 
 <script src="{{ mix('js/app.js') }}"></script>
 <script>
-    var app4 = new Vue({
-        el: '#app-4',
-        data: {
-            todos: [
-                { text: 'Learn JavaScript' },
-                { text: 'Learn Vue' },
-                { text: 'Build something awesome' }
-            ]
-        }
-    })
 
-    var app3 = new Vue({
 
-    });
+    // var myObj = {
+    //     a: 0
+    // };
+    //
+    // Object.defineProperty(myObj, 'b', {
+    //     get: function() {
+    //         return this.a + 1;
+    //     }
+    // });
+    // Object.defineProperty(myObj, 'c', {
+    //     set: function(x) {
+    //         this.a = x / 2;
+    //     }
+    // });
+    // console.log(myObj.b) // Gọi đến get và trả về a + 1, ở đây kết quả là 0 + 1 = 1
+    // myObj.c = 10;
+    // console.log(myObj.a) // Kết quả là 5
 
-    var app1 = new Vue({
-       el: '#app-1',
-       data: {
-           message: 'Hello Vue!'
-       }
-    });
-
-    var app2 = new Vue({
-        el: '#app-2',
-        data: {
-            message: 'You loaded this page on ' + new Date().toLocaleString()
-        }
-    })
-
-    var app3 = new Vue({
-        el: '#app-3',
-        data: {
-            seen: true
-        }
-    });
+    // new Vue({
+    //     el: 'app1',
+    //     data: {
+    //         a: 1
+    //     },
+    //     created: function () {
+    //         // `this` points to the vm instance
+    //         console.log('a is: ' + this.a)
+    //     }
+    // })
 </script>
 </body>
 </html>
